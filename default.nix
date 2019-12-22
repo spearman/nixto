749a13802bbbe883e36f4132bd7215c0289e1cc0
Shane Pearman <spearman@github.com>
2019-12-22 01:39:13 -0800

    rewrite default.nix

--- default.nix
+++ default.nix
@@ -1,14 +1,17 @@
 with {
-  inherit (import <nixpkgs> {}) lib makeWrapper runCommand sudo;
+  inherit (import <nixpkgs> {}) stdenv sudo;
   inherit (builtins) elemAt match readFile;
 };
 let
   version-regex = ".*NIXTO_VERSION=\"([0-9]\\.[0-9]\\.[0-9])\".*";
   version = elemAt (match version-regex (readFile ./nixto.sh)) 0;
-  name = "nixto-" + version;
 in
-runCommand name { buildInputs = [ makeWrapper sudo ]; }
-''
-  makeWrapper ${./nixto.sh} $out/bin/nixto \
-    --prefix PATH : ${lib.makeBinPath [ sudo ]}
-''
+stdenv.mkDerivation {
+  name = "nixto-" + version;
+  src = ./.;
+  buildInputs = [ sudo ];
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ./nixto.sh $out/bin/nixto
+  '';
+}
